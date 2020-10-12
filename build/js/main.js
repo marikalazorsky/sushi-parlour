4fb8866ab7b545a47d884eedab651defb7ec0295
marikalazorsky <56968573+marikalazorsky@users.noreply.github.com>
2020-10-12 09:25:40 +0200

    Js for mobile menu

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,30 @@
-window.onload = function() {
-    document.querySelector('.burger').addEventListener('click', function(){
-        document.querySelector('.burger span').classList.toggle('active');
-        document.querySelector('.header__menu nav').classList.toggle('open');
+window.addEventListener('load', init);
+
+function init() {
+
+  function toggleClasses() {
+    document.querySelector('.burger span').classList.toggle('active');
+    document.querySelector('.header__menu nav').classList.toggle('open');
+  }
+
+  document.querySelector('.burger').addEventListener('click', function(){
+    toggleClasses();
+  })
+
+  const anchors = document.querySelectorAll('.scroll-to');
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const blockID = anchor.getAttribute('href');
+      const getActiveMobileMenu = document.querySelector('.header__menu nav.open');
+      if (getActiveMobileMenu) {
+        toggleClasses();
+      }
+      document.querySelector(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
     })
+  }
 }
